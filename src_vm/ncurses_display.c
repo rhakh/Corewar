b196b5d8eda1c840e0e6b005f28a2397d89cee8a
Dmytro Telega <dtelega@e3r1p10.unit.ua>
2017-08-21 12:34:55 +0300

    fix players display in display_stats

--- src_vm/ncurses_display.c
+++ src_vm/ncurses_display.c
@@ -68,13 +68,13 @@ void		display_stats(t_data *data, WINDOW *stats_win)
 		mvwprintw(stats_win, 1, 28, "** RUNNING **");
 	mvwprintw(stats_win, 6, 3, "%-10s %6d", "Cycle:", /* count of cycles here */ 99999);
 	mvwprintw(stats_win, 7, 3, "%-10s %6d", "Processes:", /* count of processes here*/ 88888);
-	wmove(stats_win, 10, 2);
+	wmove(stats_win, 10, 3);
 	n_bot = -1;
 	while (++n_bot < data->bots_count)
 	{
 		wprintw(stats_win, "Player-%d: ", n_bot + 1);
 		wattron(stats_win, COLOR_PAIR(n_bot + 1) | A_REVERSE);
-		wprintw(stats_win, "%.57s", cur_bot->name);
+		wprintw(stats_win, "%.55s", cur_bot->name);
 		wattroff(stats_win, COLOR_PAIR(n_bot + 1) | A_REVERSE);
 		getyx(stats_win, y, x);
 		wmove(stats_win, y + 1, 10);
