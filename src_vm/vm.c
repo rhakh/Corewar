d903c8acdbd14d8735ed22f055737ce60996c586
Roman Hakh <rhakh@e3r1p9.unit.ua>
2017-09-03 13:45:25 +0300

    makefile

--- src_vm/vm.c
+++ src_vm/vm.c
@@ -71,7 +71,7 @@ int 		 infinit_loop(t_data *data)
 				ncurses_speed(data, pause);
 			if (data->one_command_mode)
 				pause = ncurses_one_cm_mode(data, pause);
-			if (pause == ' ' && !sdl_sound(MUS_BEEP))
+			if ((pause == ' ' || pause == 'p')&& !sdl_sound(MUS_BEEP))
 				while (pause == ' ')
 				pause = ncurses_global_cycle(data, pause);
 			(pause == 'n') ? (data->one_command_mode = 1) : 0;
