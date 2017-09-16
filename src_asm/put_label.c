55904189847a1a1a9dba77925ceffec493c9707d
Dmytro Telega <dtelega@e3r2p6.unit.ua>
2017-09-16 16:19:48 +0300

    norm asm

--- src_asm/put_label.c
+++ src_asm/put_label.c
@@ -10,16 +10,15 @@ void			del_put_label(t_put_label **put_label)
 	}
 }
 
-
 void			print_put_label(t_put_label *label)
 {
 	ft_printf("label_name = %s, &arg = %p, *arg = %d, curr_pc = %d",
-			  label->name, label->arg, *label->arg, label->curr_pc);
+			label->name, label->arg, *label->arg, label->curr_pc);
 }
 
 t_put_label		*new_put_label(char *name_label, int *arg, int curr_pc)
 {
-	t_put_label	*new;
+	t_put_label		*new;
 
 	if ((new = (t_put_label *)malloc(sizeof(t_put_label))) == NULL)
 		return (NULL);
@@ -30,4 +29,3 @@ t_put_label		*new_put_label(char *name_label, int *arg, int curr_pc)
 	new->curr_pc = curr_pc;
 	return (new);
 }
-
